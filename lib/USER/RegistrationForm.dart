fb2a0e94afde2e335fb56b8b7c31878fe8b85627
akashcp8943 <akashcp8943@gmail.com>
2024-02-06 11:23:39 +0530

    first commit

--- lib/USER/RegistrationForm.dart
+++ lib/USER/RegistrationForm.dart
@@ -6,7 +6,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:intl/intl.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'Dash.dart';
